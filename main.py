22f54ab36ec32c4f4de214f4a1e85b1c5186ce35
Nanfacius <95199216+Nanfacius@users.noreply.github.com>
2022-03-01 19:52:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import os,time,re,json
 from datetime import datetime
 import pandas as pd
 from time import sleep
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
 
 class Chrome_driver():
     def __init__(self,URL='https://vbdata.cn/newsList',path=os.path.join(os.getcwd(),'DATA'),driver_path=None):
@@ -70,6 +74,7 @@ class Chrome_driver():
         
     def concat_files(self,date1=None,date2=time.strftime("%Y%m%d", time.localtime()),filepath=None):#合并文件，日期从date1到date2，格式为‘YYYYmmDD’,date1若赋值'today'则合并当天新闻
         print('Concatenating files......')
+        self.filepath=filepath
         if not date1:
             date1='20000101'
         if date1=='today':
@@ -86,16 +91,52 @@ class Chrome_driver():
         df=pd.concat(dfs,ignore_index=True).drop_duplicates().sort_values(by=['Date','Time'],ascending=False)
         df.to_excel(filepath,index=False)
         print('Files contenated!')
+        
     def quit(self):
         self.driver.quit()
         print('Driver quited!')
+        
+    def send_mail(self):
+        #使用第三方邮件服务
+        mail_host="smtp.qq.com"  #设置服务器
+        mail_user="1696239338@qq.com"    #用户名
+        mail_pass="oimlkxiajuigcfjf"   #口令 
+
+        sender = '1696239338@qq.com'
+        receivers = ['1696239338@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+        #创建一个带附件的实例
+        message = MIMEMultipart()
+        message['From'] = Header("爬虫", 'utf-8')
+        message['To'] =  Header("尊敬的用户", 'utf-8')
+        subject = '今日医药新闻'
+        message['Subject'] = Header(subject, 'utf-8')
+
+        #邮件正文内容 
+        # 构造附件1
+        att1 = MIMEText(open(self.filepath, 'rb').read(), 'base64', 'utf-8')
+        att1["Content-Type"] = 'application/octet-stream'
+        # 这里的filename可以任意写，写什么名字，邮件中显示什么名字（不要使用中文）
+        att1["Content-Disposition"] = 'attachment; filename="DailyNews20220301.xlsx"'
+        message.attach(att1)
+
+        try:
+            smtpObj = smtplib.SMTP() 
+            smtpObj.connect(mail_host, 25)    # 25 为 SMTP 端口号
+            smtpObj.login(mail_user,mail_pass)  
+            smtpObj.sendmail(sender, receivers, message.as_string())
+            print("邮件发送成功")
+        except smtplib.SMTPException:
+            print("Error: 无法发送邮件")
+            
 print(os.getcwd())
 # if __name__ == '__main__':
 driver=Chrome_driver() #传递driver路径，如果driver保存在Python.exe相同目录下则可以不传递参数
 driver.get_URL()
 items,dates=driver.get_data()
-driver.process_data(items,dates)
+filepath='每日新闻合并'+time.strftime("%Y%m%d", time.localtime())+'.xlsx'
+driver.process_data(items,dates,filepath)
 driver.quit()
 driver.save_data()
 driver.concat_files('today')
-    
+
